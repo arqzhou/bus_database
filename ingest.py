84cf13c0d32a9ec6d4a98a8cf38d79a8635ee9af
moonlight <arqzhou@gmail.com>
2026-06-28 11:25:51 -0500

    Added json info to trips table in shuttle.db

--- ingest.py
+++ ingest.py
@@ -10,6 +10,7 @@ def ingest(filepath):
 
     trips = data["entity"][0:5]
     print(trips[0])
+    trips_data = []
 
     for trip in trips:
         tripId = trip["tripUpdate"]["trip"]["tripId"]
@@ -18,10 +19,30 @@ def ingest(filepath):
         routeId = trip["tripUpdate"]["trip"]["routeId"]
         directionId = trip["tripUpdate"]["trip"]["directionId"]
         vehicle = None if scheduleRelationship == "CANCELED" else trip["tripUpdate"]["vehicle"]["id"]
-        feed_timestamp = trip["tripUpdate"]["timestamp"]
+        feedTimestamp = trip["tripUpdate"]["timestamp"]
 
-        print((tripId, startDate, scheduleRelationship, routeId, directionId, vehicle, feed_timestamp))
+
+        # trip_id TEXT, start_date TEXT, feed_timestamp INTEGER, schedule_relationship TEXT, route_id TEXT, direction INTEGER, vehicle_id TEXT, PRIMARY KEY (trip_id, start_date, feed_timestamp))")
+        trips_data.append((tripId, startDate, feedTimestamp, scheduleRelationship, routeId, directionId, vehicle))
+        print((tripId, startDate, scheduleRelationship, routeId, directionId, vehicle, feedTimestamp))
+
+
+    con = sqlite3.connect("shuttle.db")
+    cur = con.cursor()
+    cur.executemany("INSERT INTO trips VALUES (?, ?, ?, ?, ?, ?, ?)", trips_data)
+    con.commit()
+    con.close()
+
+def check_tables():
+    con = sqlite3.connect("shuttle.db")
+    cur = con.cursor()
+
+    for row in cur.execute("SELECT trip_id, start_date, feed_timestamp FROM trips ORDER BY feed_timestamp"):
+        print(row)
+    con.close()
+    
 
 if __name__ == "__main__":
     db.init_db()
     ingest("tripupdates.json")
+    check_tables()
