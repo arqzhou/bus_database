f5180271b757791a7e9795a7a8b5e9b83f614a47
moonlight <arqzhou@gmail.com>
2026-06-30 12:15:04 -0500

    Added historical cancelation trends

--- queries.py
+++ queries.py
@@ -76,7 +76,43 @@ def most_stop_updates():
         print(f"Route ID: {row[0]}\t has {row[1]}\t stop updates, on average.")
 
     con.close()
+
+def cancel_trends():
+    con = sqlite3.connect("shuttle.db")
+    cur = con.cursor()
+
+    query = """
+        SELECT
+            route_id,
+            COUNT(CASE WHEN schedule_relationship = 'CANCELED' THEN 1 END) AS canceled_trips,
+            COUNT(*) AS total_trips,
+            COUNT(CASE WHEN schedule_relationship = 'CANCELED' AND feed_timestamp = '1782235081' THEN 1 END) AS canceled_earlier_trips,
+            COUNT(CASE WHEN feed_timestamp = '1782235081' THEN 1 END) AS total_earlier_trips,
+            ROUND(SUM(CASE WHEN schedule_relationship = 'CANCELED' AND feed_timestamp = '1782235081' THEN 1.0 ELSE 0.0 END) / SUM(CASE WHEN feed_timestamp = '1782235081' THEN 1.0 ELSE 0.0 END), 2) AS early_cancel_rate,
+            COUNT(CASE WHEN schedule_relationship = 'CANCELED' AND feed_timestamp = '1782835557' THEN 1 END) AS canceled_later_trips,
+            COUNT(CASE WHEN feed_timestamp = '1782835557' THEN 1 END) AS total_later_trips,
+            ROUND(SUM(CASE WHEN schedule_relationship = 'CANCELED' AND feed_timestamp = '1782835557' THEN 1.0 ELSE 0.0 END) / SUM(CASE WHEN feed_timestamp = '1782835557' THEN 1.0 ELSE 0.0 END), 2) AS late_cancel_rate,
+            ROUND(SUM(CASE WHEN schedule_relationship = 'CANCELED' AND feed_timestamp = '1782835557' THEN 1.0 ELSE 0.0 END) / SUM(CASE WHEN feed_timestamp = '1782835557' THEN 1.0 ELSE 0.0 END) - SUM(CASE WHEN schedule_relationship = 'CANCELED' AND feed_timestamp = '1782235081' THEN 1.0 ELSE 0.0 END) / SUM(CASE WHEN feed_timestamp = '1782235081' THEN 1.0 ELSE 0.0 END), 2) AS change_in_rate
+        FROM trips
+        GROUP BY route_id
+        HAVING change_in_rate IS NOT NULL
+        ORDER BY change_in_rate DESC;
+    """
+
+    cur.execute(query)
+    results = cur.fetchall()
+
+    print("Largest Increases in Cancelation Rate:")
+    for row in results[0:5]:
+        print(f"Route ID: {row[0]}\twith a change of {row[9]}\t(from {row[5]}\tto {row[8]})")
+
+    print("Largest Decreases in Cancelation Rate:")
+    for row in results[-1:-6:-1]:
+        print(f"Route ID: {row[0]}\twith a change of {row[9]}\t(from {row[5]}\tto {row[8]})")
     
 if __name__ == "__main__":
+    print("TOTAL STATS:")
     cancellation_rate()
     most_stop_updates()
+    print("Historical Trends")
+    cancel_trends()
